e33a1ebfad7efd043745ca9b9a170142cde3800f
Snehasish Ghosh Dastidar <kuttulr1996@gmail.com>
2023-02-20 01:11:08 +0530

    dep

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const app = express();
 const {connection} = require("./config/db")
 const {userRoute} = require("./routers/user.router")
@@ -6,6 +7,7 @@ const {noteRouter} = require("./routers/notes.route")
 const {authenticate} = require("./middlewares/authenticate.middleware")
 
 app.use(express.json())
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("Hi there")
